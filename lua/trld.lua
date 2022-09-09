cda5dffb5eaf0805f759355b958127ec7354961c
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-09-09 19:50:09 -0300

    chore: Format with stylua

--- lua/trld.lua
+++ lua/trld.lua
@@ -7,55 +7,59 @@ local u = require 'trld.utils'
 local M = {}
 
 function TRLDShow(opts, bufnr, line_nr, client_id)
-    bufnr = bufnr or 0
-    line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
-    opts = opts or { ['lnum'] = line_nr }
+   bufnr = bufnr or 0
+   line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
+   opts = opts or { ['lnum'] = line_nr }
 
-    local ns = vim.api.nvim_create_namespace "trld"
-    local diag_ns = vim.diagnostic.get_namespace(ns)
+   local ns = vim.api.nvim_create_namespace 'trld'
+   local diag_ns = vim.diagnostic.get_namespace(ns)
 
-    local line_diags = vim.diagnostic.get(bufnr, opts)
+   local line_diags = vim.diagnostic.get(bufnr, opts)
 
-    -- clear and exit namespace if line has no diagnostics
-    if vim.tbl_isempty(line_diags) then
-        if diag_ns.user_data.diags then
-            vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
-        end
-        return
-    end
+   -- clear and exit namespace if line has no diagnostics
+   if vim.tbl_isempty(line_diags) then
+      if diag_ns.user_data.diags then
+         vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+      end
+      return
+   end
 
-    if diag_ns.user_data.last_line_nr == line_nr and diag_ns.user_data.diags then
-        return
-    end
+   if diag_ns.user_data.last_line_nr == line_nr and diag_ns.user_data.diags then
+      return
+   end
 
-    diag_ns.user_data.diags = true
-    diag_ns.user_data.last_line_nr = line_nr
+   diag_ns.user_data.diags = true
+   diag_ns.user_data.last_line_nr = line_nr
 
-    u.display_diagnostics(line_diags, bufnr, ns, c.config.position)
+   u.display_diagnostics(line_diags, bufnr, ns, c.config.position)
 end
 
 function TRLDHide(opts, bufnr, line_nr, client_id)
-    bufnr = bufnr or 0
-    local namespace = vim.api.nvim_get_namespaces()['trld'];
-    if namespace == nil then return end
-    local ns = vim.diagnostic.get_namespace(namespace)
-    if ns.user_data.diags then
-        vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
-    end
-    ns.user_data.diags = false
+   bufnr = bufnr or 0
+   local namespace = vim.api.nvim_get_namespaces()['trld']
+   if namespace == nil then
+      return
+   end
+   local ns = vim.diagnostic.get_namespace(namespace)
+   if ns.user_data.diags then
+      vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
+   end
+   ns.user_data.diags = false
 end
 
 M.setup = function(cfg)
-    -- exit if user configs are invalid
-    if not c.validate_config(cfg) then return end
+   -- exit if user configs are invalid
+   if not c.validate_config(cfg) then
+      return
+   end
 
-    -- override configs with user configs
-    c.override_config(cfg or {})
+   -- override configs with user configs
+   c.override_config(cfg or {})
 
-    if c.config.auto_cmds then
-        vim.cmd [[ autocmd! CursorHold,CursorHoldI * lua TRLDShow() ]]
-        vim.cmd [[ autocmd! CursorMoved,CursorMovedI * lua TRLDHide() ]]
-    end
+   if c.config.auto_cmds then
+      vim.cmd [[ autocmd! CursorHold,CursorHoldI * lua TRLDShow() ]]
+      vim.cmd [[ autocmd! CursorMoved,CursorMovedI * lua TRLDHide() ]]
+   end
 end
 
 return M
--- lua/trld/config.lua
+++ lua/trld/config.lua
@@ -2,31 +2,30 @@ local M = {}
 
 -- default config
 M.default_config = {
-    position = "top",
-    auto_cmds = true,
-    highlights = {
-        error = "DiagnosticFloatingError",
-        warn = "DiagnosticFloatingWarn",
-        info = "DiagnosticFloatingInfo",
-        hint = "DiagnosticFloatingHint",
-    },
-    formatter = function(diag)
-        local u = require 'trld.utils'
-        local diag_lines = {}
+   position = 'top',
+   auto_cmds = true,
+   highlights = {
+      error = 'DiagnosticFloatingError',
+      warn = 'DiagnosticFloatingWarn',
+      info = 'DiagnosticFloatingInfo',
+      hint = 'DiagnosticFloatingHint',
+   },
+   formatter = function(diag)
+      local u = require 'trld.utils'
+      local diag_lines = {}
 
-        for line in diag.message:gmatch("[^\n]+") do
-            line = line:gsub('[ \t]+%f[\r\n%z]', '')
-            table.insert(diag_lines, line)
-        end
+      for line in diag.message:gmatch '[^\n]+' do
+         line = line:gsub('[ \t]+%f[\r\n%z]', '')
+         table.insert(diag_lines, line)
+      end
 
-        local lines = {}
-        for _, diag_line in ipairs(diag_lines) do
-            table.insert(lines, { { diag_line .. ' ', u.get_hl_by_serverity(diag.severity) } })
-        end
+      local lines = {}
+      for _, diag_line in ipairs(diag_lines) do
+         table.insert(lines, { { diag_line .. ' ', u.get_hl_by_serverity(diag.severity) } })
+      end
 
-        return lines
-    end,
-
+      return lines
+   end,
 }
 
 -- config
@@ -34,13 +33,13 @@ M.config = {}
 
 -- return bool indicating in config is valid
 M.validate_config = function(cfg)
-    -- TODO: implement config validation with vim.notify() logging
-    return true
+   -- TODO: implement config validation with vim.notify() logging
+   return true
 end
 
 -- override the default config with user configs
 M.override_config = function(cfg)
-    M.config = vim.tbl_deep_extend("force", M.default_config, cfg or {})
+   M.config = vim.tbl_deep_extend('force', M.default_config, cfg or {})
 end
 
 return M
