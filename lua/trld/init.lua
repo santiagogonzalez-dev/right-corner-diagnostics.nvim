abcab4b36381553bf67115540df42ad5112304bc
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-09-10 14:09:49 -0300

    Renaming
    Deleted config.lua
    
    Include configs in init.lua

--- lua/trld/init.lua
+++ lua/trld/init.lua
@@ -1,20 +1,27 @@
-local config = require 'trld.config'
+-- local config = require 'trld.config'
 local M = {}
+
+M.config = {}
+
+M.default_config = {
+   position = 'top',
+   auto_cmds = true,
+}
 
 function M.show(opts, bufnr, line_nr)
    bufnr = bufnr or 0
    line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
    opts = opts or { ['lnum'] = line_nr }
 
-   local ns = vim.api.nvim_create_namespace 'trld'
-   local diag_ns = vim.diagnostic.get_namespace(ns)
+   local ns_trld = vim.api.nvim_create_namespace 'trld'
+   local diag_ns = vim.diagnostic.get_namespace(ns_trld)
 
    local line_diags = vim.diagnostic.get(bufnr, opts)
 
    -- clear and exit namespace if line has no diagnostics
    if vim.tbl_isempty(line_diags) then
       if diag_ns.user_data.diags then
-         vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+         vim.api.nvim_buf_clear_namespace(bufnr, ns_trld, 0, -1)
       end
       return
    end
@@ -26,7 +33,7 @@ function M.show(opts, bufnr, line_nr)
    diag_ns.user_data.diags = true
    diag_ns.user_data.last_line_nr = line_nr
 
-   require('trld.utils').display_diagnostics(line_diags, bufnr, ns, config.config.position)
+   require('trld.utils').display_diagnostics(line_diags, bufnr, ns_trld, M.config.position)
 end
 
 function M.hide(bufnr)
@@ -44,12 +51,13 @@ function M.hide(bufnr)
    ns_trld.user_data.diags = false
 end
 
----@param user_configs table
-M.setup = function(user_configs)
-   config.merge_configs(user_configs or {})
+---@param user_config table
+M.setup = function(user_config)
+   -- config.merge_configs(user_configs or {})
+   M.config = vim.tbl_deep_extend('force', M.default_config, user_config or {})
 
    -- Enable autocmds?
-   if config.config.auto_cmds then
+   if M.config.auto_cmds then
       local au_trld = vim.api.nvim_create_augroup('trld', { clear = false })
 
       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
